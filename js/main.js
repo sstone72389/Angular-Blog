41a908da72f4516944850031eac616a348d0d080
sstone72389 <sstone72389@gmail.com>
2017-09-29 17:30:44 -0400

    continues with

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var app = angular.module('myApp', []);
 
-app.directive('prettyprint', function() {
-    return {
-        restrict: 'C',
-        link: function postLink(scope, element, attrs) {
-              element.html(prettyPrintOne(scope.dom));
-        }
-    };
+// app.directive('prettyprint', function() {
+//     return {
+//         restrict: 'C',
+//         link: function postLink(scope, element, attrs) {
+//               element.html(prettyPrintOne(scope.dom));
+//         }
+//     };
+// });
+
+app.run(function($rootScope) {
+  $rootScope.name = "Angular Student";
 });
