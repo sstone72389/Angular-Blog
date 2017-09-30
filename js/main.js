8a7e46b26d38facb65bea3f1c1495d8b1191166b
“sstone72389” <sstone72389@gmail.com>
2017-09-30 14:30:09 -0400

    Adds styling to <code> tags
    Adds styling to <code> tags to allow easier readability.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 var app = angular.module('myApp', []);
 
-// app.directive('prettyprint', function() {
-//     return {
-//         restrict: 'C',
-//         link: function postLink(scope, element, attrs) {
-//               element.html(prettyPrintOne(scope.dom));
-//         }
-//     };
-// });
-
 app.run(function($rootScope) {
   $rootScope.name = "Angular Student";
 });
