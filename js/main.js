448c11624a628a02bf0524aad52563a05df29400
“sstone72389” <sstone72389@gmail.com>
2017-09-30 19:06:50 -0400

    Continues to work with $scopes and adds image
    Continues to work with $scopes and adds image of prototypical
    inheritance associated with javascript and Angular.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ var app = angular.module('myApp', []);
 app.run(function($rootScope) {
   $rootScope.name = "Angular Student"
 });
+
+app.controller('myFirstController', function($scope){
+  $scope.student = {
+    name: "Bojack Horseman"
+  };
+});
