03c885a7aa2a15562cf31512424ef4b14d207775
sstone72389 <sstone72389@gmail.com>
2017-10-02 16:18:35 -0400

    adds animations to view changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate']);
 
 // configure our routes
 app.config(function($routeProvider) {
@@ -12,23 +12,43 @@ app.config(function($routeProvider) {
 
         // route for the getting started page
         .when('/intro', {
-            templateUrl : 'pages/intro.html'
+            templateUrl : 'pages/intro.html',
+            controller: 'mainController'
         })
 
         // route for the javascript page
         .when('/javascript', {
-            templateUrl : 'pages/js.html'
+            templateUrl : 'pages/js.html',
+            controller: 'aboutController'
         })
 
         // route for the scopes page
         .when('/scopes', {
-            templateUrl : 'pages/scopes.html'
+            templateUrl : 'pages/scopes.html',
+            controller: 'contactController'
         })
 
         .otherwise({
 			       redirectTo: '/intro'
 		    });
 });
+
+// CONTROLLERS ============================================
+// home page controller
+app.controller('mainController', function($scope) {
+    $scope.pageClass = 'page-home';
+});
+
+// about page controller
+app.controller('aboutController', function($scope) {
+    $scope.pageClass = 'page-about';
+});
+
+// contact page controller
+app.controller('contactController', function($scope) {
+    $scope.pageClass = 'page-contact';
+});
+
 
 app.run(function($rootScope) {
   $rootScope.name = "Angular Student"
